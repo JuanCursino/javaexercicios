92cead9bea8b9c0001f83d3bd03033c0fc1a27f4
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2024-03-06 22:54:46 -0300

    Tarefa2
    atividade 1 de java em sala

---
+++ linguagemdeprogramação/Javainicio/Seres_vivos/src/Ser_vivo.java
@@ -0,0 +1,44 @@
+//metodos
+public class Ser_vivo {
+    //atributos
+    private String life;
+    private int quantidade_patas;
+    private String tipo ;
+ //métodos
+   public Ser_vivo()
+   {
+       this.quantidade_patas=4;
+   }
+    public void setTipo(String t){
+        this.tipo=t;
+    }
+    public String mostrar_especie(){
+        return(this.tipo);
+    }
+    public void mostrar_quantidades_membros(){
+        System.out.println(this.quantidade_patas);
+    }
+
+    public boolean show_life(int i){
+        boolean vivo = true;
+        if(i%2==0) vivo = false;
+        return vivo;
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        Ser_vivo s1;
+        s1=new Ser_vivo();
+        s1.show_life(1);
+        s1.setTipo("Tigre");
+        s1.mostrar_quantidades_membros();
+
+        Ser_vivo s2=new Ser_vivo();
+        s2.show_life(2);
+        s2.setTipo("Leão");
+        s2.mostrar_quantidades_membros();
+        System.out.println("Especie 1: " + s1.mostrar_especie());
+        System.out.println("Especie 2: " + s2.mostrar_especie());
+
+    }
+}
